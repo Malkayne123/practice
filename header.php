1a73923d7062007a4f8461474a8365752db6f58b
Malkayne123 <owosenikayode123@gmail.com>
2023-09-02 17:42:13 +0100

    this is the rest of the codes

---
+++ header.php
@@ -0,0 +1,52 @@
+<?php
+
+<div class="header-area">
+    <div class="main-header ">
+        <div class="header-bottom  header-sticky">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <!-- Logo -->
+                    <div class="col-xl-2 col-lg-2">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-10 col-lg-10">
+                        <div class="menu-wrapper  d-flex align-items-center justify-content-end">
+                            <!-- Main-menu -->
+                            <div class="main-menu d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">                                                                                          
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="program.html">Fundraise For</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Medical</a></li>
+                                                <li><a href="blog_details.html">Memorial</a></li>
+                                                <li><a href="elements.html">Emergency</a></li>
+                                                <li><a href="elements.html">Charity</a></li>
+                                                <li><a href="elements.html">Education</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="events.html">social events </a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!-- Header-btn -->
+                            <div class="header-right-btn d-none d-lg-block ml-10">
+                                <a href="contact.html" class="btn header-btn">Donate</a>
+                            </div>
+                        </div>
+                    </div> 
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+?>
